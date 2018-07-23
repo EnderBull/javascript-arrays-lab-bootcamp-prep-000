5668e8483d19a371a0dedbefb08c25be7eb67bde
Simon A Bull <simonbull1@gmail.com>
2018-07-23 15:59:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function prependKitten(name){
 
 function removeLastKitten(){
   
-  return kittens.slice(kittens.length)
+  return kittens.slice(kittens.length-1)
 }
 
 function removeFirstKitten(){
