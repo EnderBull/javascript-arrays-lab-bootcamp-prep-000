93d14a3e48152c83cf068eee131c6e42f14c2970
Simon A Bull <simonbull1@gmail.com>
2018-07-23 16:07:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,22 +41,3 @@ function removeFirstKitten(){
   return kittens.slice(1)
 }
 
-/*
-
-  describe('removeLastKitten()', function() {
-    it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      expect(removeLastKitten()).toEqual(["Milo", "Otis"])
-
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
-
-  describe('removeFirstKitten()', function() {
-    it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
-
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
-
-})*/
